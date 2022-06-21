ff5ccb5de628ce74dc78c282b0e85bdb7d83104d
rahul-lt <rahulm@lambdatest.com>
2022-06-21 13:43:54 +0530

    added a suite

--- src/test/unit/App.test.js
+++ src/test/unit/App.test.js
@@ -1,20 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from '../../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
-test('renders learn react', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
-test('renders random text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Angular/i);
-  expect(linkElement).toBeInTheDocument();
+describe('my beverage', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('renders learn react', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('renders random text', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Angular/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
