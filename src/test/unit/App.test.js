1e202ce652f8fd39d9898b028fdd9553a61f1b3e
rahul-lt <rahulm@lambdatest.com>
2022-06-23 13:43:03 +0530

    reand

--- src/test/unit/App.test.js
+++ src/test/unit/App.test.js
@@ -3,21 +3,18 @@ import App from '../../App';
 
 test('renders learn react link', () => {
   render(<App />);
-  console.log('Yo Yo');
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react', () => {
   render(<App />);
-  console.log('Yo Yo');
   const linkElement = screen.getByText(/react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders random text', () => {
   render(<App />);
-  console.log('Yo Yo');
   const linkElement = screen.getByText(/Angular/i);
   expect(linkElement).toBeInTheDocument();
 });
