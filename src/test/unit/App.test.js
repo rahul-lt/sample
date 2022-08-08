bb6ec6851877dfdb854e357474b41014a9943cf1
rahul-lt <rahulm@lambdatest.com>
2022-08-08 12:01:08 +0530

    asdasd

--- src/test/unit/App.test.js
+++ src/test/unit/App.test.js
@@ -67,10 +67,6 @@ test('renders lesarn 12f2311232', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders lesarn asd12f2311232', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/react/i);
-  expect
 
 
 
